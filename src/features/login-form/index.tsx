07a6f3dd9031823c91fcef875afe3b7c18c98e5c
pampaka <pampakadev@yandex.ru>
2024-06-30 18:33:09 +0300

    fix login form

--- src/features/login-form/index.tsx
+++ src/features/login-form/index.tsx
@@ -37,7 +37,7 @@ export const LoginForm = ({ ...props }: LoginFormProps) => {
 				{...registers.password}
 			/>
 			<Checkbox label="Запомнить пароль" {...registers.rememberUser} />
-			<ErrorMessage error={errors.root?.serverError?.message || 'asdasdasd'} />
+			<ErrorMessage error={errors.root?.serverError?.message} />
 			<Button className={style.submit} disabled={isSubmitting}>
 				{isSubmitting ? <Loader /> : 'Войти'}
 			</Button>
